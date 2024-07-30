77eef2cf6ffa8c6dd30ac96cda23085fdfb5abf1
Joowon Lim <m4rinesnow34@gmail.com>
2024-07-31 01:20:02 +0900

    Config: 통합테스트 제거

--- src/test/java/com/dang/travel/TravelApplicationTests.java
+++ src/test/java/com/dang/travel/TravelApplicationTests.java
@@ -1,13 +1,5 @@
 package com.dang.travel;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class TravelApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
