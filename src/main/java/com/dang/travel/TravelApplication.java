872fd95f5f37f36d94e1a2c722c2bf8ea114455f
Joowon Lim <m4rinesnow34@gmail.com>
2024-08-04 23:16:30 +0900

    Test: pr action test

--- src/main/java/com/dang/travel/TravelApplication.java
+++ src/main/java/com/dang/travel/TravelApplication.java
@@ -1,7 +1,7 @@
 package com.dang.travel;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class TravelApplication {
