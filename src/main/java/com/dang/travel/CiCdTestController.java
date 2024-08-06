bd7139f4cde82e7a378a1970aba322506bd1c2f2
Joowon Lim <m4rinesnow34@gmail.com>
2024-08-06 17:24:14 +0900

    Style: 컨벤션 준수

--- src/main/java/com/dang/travel/CiCdTestController.java
+++ src/main/java/com/dang/travel/CiCdTestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
-public class CICDTestController {
+public class CiCdTestController {
 	@GetMapping
 	public String getServerIp() {
 		try {
