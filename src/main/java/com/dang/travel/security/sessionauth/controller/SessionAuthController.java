f61723aa5f93aeab5bb751d3d39ba74a600b1d38
Cho hyun-sik <78861124+1223v@users.noreply.github.com>
2024-09-30 03:49:52 +0900

    Fix: 시큐리티 필터 swagger 재설정 (#17)
    * Config: 예외 처리 구축
    
    * Feat: 회원 도메인 구축
    
    * Test: 회원 도메인 닉네임 테스트
    
    * Feat: 회원 관리 기능 추가
    
    * Fix: 스프링 시큐리티 필터 순서 오류 수정
    
    * Style: 코드 포멧팅
    
    * Fix: SecurityConfig swagger 필터 수정

--- src/main/java/com/dang/travel/security/sessionauth/controller/SessionAuthController.java
+++ src/main/java/com/dang/travel/security/sessionauth/controller/SessionAuthController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @Slf4j
 public class SessionAuthController {
