f14976abfc750546dae8ddcfdc06c8d347bd9b0c
Joowon Lim <m4rinesnow34@gmail.com>
2024-11-05 23:25:12 +0900

    Feat: 결제 생성 프로덕트id추가

--- src/main/java/com/dang/travel/payment/service/dto/request/CreateTosspaymentRequest.java
+++ src/main/java/com/dang/travel/payment/service/dto/request/CreateTosspaymentRequest.java
@@ -2,12 +2,15 @@ package com.dang.travel.payment.service.dto.request;
 
 import com.dang.travel.member.domain.Member;
 import com.dang.travel.payment.domain.TossPayment;
+import com.dang.travel.product.domain.Product;
 
 public record CreateTosspaymentRequest(
-	Integer amount
+	Integer amount,
+	Long productId
 ) {
-	public TossPayment toEntity(String orderName, Member member) {
+	public TossPayment toEntity(String orderName, Member member, Product product) {
 		return new TossPayment().builder()
+			.product(product)
 			.member(member)
 			.amount(amount)
 			.orderName(orderName)
